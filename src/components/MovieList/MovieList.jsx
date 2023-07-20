1a102f9833c67e5beb780deb22d1c2338446a39a
pttkachuk <petrotkachuk6@gmail.com>
2023-07-20 14:41:05 +0200

    create MovieList.jsx

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,34 @@
+import CastLoader from 'components/CastLoader/CastLoader';
+import PropTypes from 'prop-types';
+import { NavLink, useLocation } from 'react-router-dom';
+
+const MovieList = ({ tranding, loading }) => {
+    const location = useLocation();
+    return (
+        <>
+            {loading ? (<CastLoader />) : (<ul>
+                {tranding.map(({ id, title, name }) => {
+                    <li key={id}>
+                        <NavLink to={`movies/${id}`} state={{ from: location.pathname }} >
+                            {title || name}
+                        </NavLink>
+                    </li>
+                })}
+            </ul>
+            )}
+        </>
+    );
+};
+
+export default MovieList;
+
+MovieList.propTypes = {
+    tranding: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            title: PropTypes.string,
+            name: PropTypes.string,
+        })
+    ),
+    loading: PropTypes.bool,
+};
