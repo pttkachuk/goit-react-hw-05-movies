697d7a635c01d12e09dce89167dd279777cc6b87
pttkachuk <petrotkachuk6@gmail.com>
2023-07-21 09:07:57 +0200

    create SharedLayout file

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,20 @@
+import CastLoader from 'components/CastLoader/CastLoader';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+
+const SharedLayout = () => {
+    return (
+        <div>
+            <header>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/movies">Movies</NavLink>
+            </header>
+            <Suspense fallback={<CastLoader />}>
+                <Outlet />
+            </Suspense>
+        </div>
+    );
+};
+
+export default SharedLayout;
