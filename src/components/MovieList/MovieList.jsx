8f40362e0b036b4bc39a5cd4b759c597335d880c
pttkachuk <petrotkachuk6@gmail.com>
2023-07-21 10:14:05 +0200

    upd movielist

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,32 +3,35 @@ import PropTypes from 'prop-types';
 import { NavLink, useLocation } from 'react-router-dom';
 
 const MovieList = ({ tranding, loading }) => {
-    const location = useLocation();
-    return (
-        <>
-            {loading ? (<CastLoader />) : (<ul>
-                {tranding.map(({ id, title, name }) => {
-                    <li key={id}>
-                        <NavLink to={`movies/${id}`} state={{ from: location }} >
-                            {title || name}
-                        </NavLink>
-                    </li>
-                })}
-            </ul>
-            )}
-        </>
-    );
+  const location = useLocation();
+  return (
+    <>
+      {loading ? (
+        <CastLoader />
+      ) : (
+        <ul>
+          {tranding.map(({ id, title, name }) => {
+            <li key={id}>
+              <NavLink to={`movies/${id}`} state={{ from: location }}>
+                {title || name}
+              </NavLink>
+            </li>;
+          })}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default MovieList;
 
 MovieList.propTypes = {
-    tranding: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            title: PropTypes.string,
-            name: PropTypes.string,
-        })
-    ),
-    loading: PropTypes.bool,
+  tranding: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
+  loading: PropTypes.bool,
 };
